a0c080cd5e7736927428ea636dfec78d19a29029
ronnylov <30211713+ronnylov@users.noreply.github.com>
2020-06-10 23:50:41 +0200

    Added flutter_map

--- lib/screens/providers_list_screen.dart
+++ lib/screens/providers_list_screen.dart
@@ -10,6 +10,8 @@ import 'package:provider/provider.dart';
 
 import '../providers/exit_node_providers.dart';
 import '../widgets/exit_node_provider_tile.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 // These were used for testing detection of device external IP address
 // When pressing floatingactionbutton
@@ -58,13 +60,60 @@ class _ProviderListScreenState extends State<ProviderListScreen> {
                   ? Center(
                       child: const Text('No exit node available.'),
                     )
-                  : Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: ListView.builder(
-                        itemCount: _currentNodeList.length,
-                        itemBuilder: (context, index) => ExitNodeProviderTile(
-                            currentNode: _currentNodeList[index]),
-                      ),
+                  : Column(
+                      children: <Widget>[
+                        Card(
+                          elevation: 4,
+                          margin: const EdgeInsets.all(6),
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Container(
+                            height: 200,
+                            child: FlutterMap(
+                              options: MapOptions(
+                                center: LatLng(51.5, -0.09),
+                                zoom: 1.0,
+                              ),
+                              layers: [
+                                TileLayerOptions(
+                                    urlTemplate:
+                                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                                    subdomains: ['a', 'b', 'c']),
+                                MarkerLayerOptions(
+                                  markers: [
+                                    Marker(
+                                      width: 28.0,
+                                      height: 28.0,
+                                      point: LatLng(51.5, -0.09),
+                                      builder: (ctx) => Container(
+                                        child: Icon(
+                                          Icons.home,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ), // Add some vertical spacing
+                        Expanded(
+                          // Was necessary to wrap this in Expanded
+                          // to avoid error in Column
+                          child: ListView.builder(
+                            itemCount: _currentNodeList.length,
+                            itemBuilder: (context, index) =>
+                                ExitNodeProviderTile(
+                                    currentNode: _currentNodeList[index]),
+                          ),
+                        ),
+                      ],
                     );
             }
           },
